00869664f2f401e6f41b2e42331d63efeb979fa7
kelsie kidd <kelsiekidd93@gmail.com>
2022-07-22 01:26:18 -0500

    added when statement

--- app.js
+++ app.js
@@ -105,6 +105,24 @@ const promptUser = () => {
       },
       {
         type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+          if (confirmAbout) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+    },
+      {
+        type: 'confirm',
         name: 'feature',
         message: 'Would you like to feature this project?',
         default: false
